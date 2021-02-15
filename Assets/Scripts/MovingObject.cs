916fc5259213dd5dd071115b2afdc2eae78ffef3
mitrokosta <mitrokosta@gmail.com>
2021-02-15 22:54:56 +0300

    Fix issues

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,49 +17,28 @@ public class MovingObject : MonoBehaviour
     [SerializeField]
     private float mass; // масса тела вокруг которого крутимся
 
-    private const float Tolerance = 0.1f;
-    
+    private const float Tolerance = 5.0f; // мин радиус из которого считается макс ускорение - если в него зашли, то попали в центр поля
+    private float MaxAcceleration; // макс ускорение
+
+    private void Start()
+    {
+        MaxAcceleration = gravitation * mass / (Tolerance * Tolerance);
+    }
+
     private void FixedUpdate()
     {
-        var diff = center - transform.position;
-        if (diff.magnitude < Tolerance)
-        {
-            speed = Vector3.zero;
-            transform.Translate(diff);
-            return;
-        }
-        
         // сначала расчет ускорения
+        var diff = center - transform.position;
         var acceleration = gravitation * mass * diff.normalized / diff.sqrMagnitude;
-        // теперь передвижение
-        var delta = speed * Time.deltaTime + acceleration * (Time.deltaTime * Time.deltaTime) / 2;
-        // проверим что не прошли через центр
-        if (PointToSegmentDistance(center, transform.position, transform.position + delta) < Tolerance)
+        // проверка на заход в центр поля
+        if (acceleration.magnitude > MaxAcceleration)
         {
-            speed = Vector3.zero;
-            acceleration = Vector3.zero;
-            delta = diff;
+            acceleration = acceleration.normalized * MaxAcceleration;
         }
-        
+        // теперь апдейты
+        var delta = speed * Time.deltaTime + acceleration * (Time.deltaTime * Time.deltaTime) / 2;
         transform.Translate(delta);
         speed += acceleration * Time.deltaTime;
     }
 
-    // расстояние от точки c до отрезка ab
-    private static float PointToSegmentDistance(Vector3 c, Vector3 a, Vector3 b)
-    {
-        var ab = b - a;
-        var ac = c - a;
-        var diff = Vector3.Dot(ab, ac) / ab.sqrMagnitude;
-        if (diff > 1)
-        {
-            diff = 1;
-        }
-        else if (diff < 0)
-        {
-            diff = 0;
-        }
-
-        return (a + diff * ab - c).magnitude;
-    }
 }
