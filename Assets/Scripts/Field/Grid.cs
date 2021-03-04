5afe3cc9ddb0012eec2e1008eef868d41f65e3a1
mitrokosta <mitrokosta@gmail.com>
2021-03-04 14:57:51 +0300

    merge pathfinder

--- Assets/Scripts/Field/Grid.cs
+++ Assets/Scripts/Field/Grid.cs
@@ -1,27 +1,38 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Field
 {
     public class Grid
     {
         private Node[,] m_Nodes;
+
         private int m_Width;
         private int m_Height;
 
-        public Grid(int width, int height)
+        private Pathfinder m_Pathfinding;
+
+        public int Width => m_Width;
+        public int Height => m_Height;
+
+        public Grid(int width, int height, Vector3 offset, float nodeSize, Vector2Int target)
         {
             m_Width = width;
             m_Height = height;
-
+            
             m_Nodes = new Node[m_Width, m_Height];
 
-            for (int i = 0; i < m_Nodes.GetLength(0); ++i)
+            for (int i = 0; i < m_Nodes.GetLength(0); i++)
             {
                 for (int j = 0; j < m_Nodes.GetLength(1); j++)
                 {
-                    m_Nodes[i, j] = new Node();
+                    m_Nodes[i, j] = new Node(offset + new Vector3(i + .5f, 0, j + .5f) * nodeSize);
                 }
             }
+            
+            m_Pathfinding = new Pathfinder(this, target);
+            
+            m_Pathfinding.UpdateField();
         }
 
         public Node GetNode(Vector2Int coordinate)
@@ -42,6 +53,22 @@ namespace Field
             }
             
             return m_Nodes[i, j];
+        }
+
+        public IEnumerable<Node> EnumerateAllNodes()
+        {
+            for (int i = 0; i < m_Width; i++)
+            {
+                for (int j = 0; j < m_Height; j++)
+                {
+                    yield return GetNode(i, j);
+                }
+            }
+        }
+
+        public void UpdatePathfinding()
+        {
+            m_Pathfinding.UpdateField();
         }
     }
 }
